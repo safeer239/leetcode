2b10641c366eec16bdbb2849b488d96ed285e673
Mohammed Safeer <139971620+safeer239@users.noreply.github.com>
2024-07-31 11:06:53 +0530

    Time: 106 ms (96.3%), Space: 68.6 MB (79.53%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    let obj={}
+    for(i=0;i<this.length;i++){
+        let key=fn(this[i])
+    if(obj.hasOwnProperty(key)){
+        obj[key].push(this[i])
+    }
+    else{
+        obj[key]=[this[i]]
+    }
+    }
+    return obj
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
