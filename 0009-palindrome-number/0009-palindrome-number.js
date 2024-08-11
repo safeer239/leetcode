8cd394bcfaed8305199686fbbd782626a9605705
Mohammed Safeer <139971620+safeer239@users.noreply.github.com>
2024-08-11 11:17:54 +0530

    Time: 139 ms (41.4%), Space: 59.1 MB (9.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,6 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if(x<0) return false
-   return x === +x.toString().split("").reverse().join("")
+   return x<0 ? false :x === +x.toString().split("").reverse().join("")
 };
