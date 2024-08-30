9363f299f54d1d6ec97f805ebe716a35747e95b5
Mohammed Safeer <139971620+safeer239@users.noreply.github.com>
2024-08-30 10:06:59 +0530

    Time: 83 ms (32.34%), Space: 56.9 MB (64.51%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    if(s===""){
+        return ""
+    }
+    else{
+        return s.reverse()
+    }
+};
