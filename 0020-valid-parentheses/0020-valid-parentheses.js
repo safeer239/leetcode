7f1ebaae6f49157ba3a0d4921e613a8d6768f527
Mohammed Safeer <139971620+safeer239@users.noreply.github.com>
2024-09-02 09:39:36 +0530

    Time: 64 ms (24%), Space: 49.2 MB (97.12%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack =[]
+    for(let i= 0;i<s.length;i++){
+        const char=s[i]
+        if(char==="(" ||char==="{" || char==="["){
+            stack.push(char)
+        }else if(char===")" ||char==="}" || char==="]"){
+            if(isEmpty(stack)){
+                return false
+            }
+            const top=stack.pop()
+            if(
+            (char===")"&& top!=="(") ||
+            (char==="}"&& top!=="{") ||
+            (char==="]"&& top!=="[") 
+            ){
+                return false
+            }
+        }
+    }
+    return isEmpty(stack)
+};
+
+function isEmpty(stack){
+    return stack.length===0
+}
