87ffe893fc521799b56ae25c688d19f879848a58
Mohammed Safeer <139971620+safeer239@users.noreply.github.com>
2024-10-19 14:33:17 +0530

    Time: 0 ms (100%), Space: 48.4 MB (96.79%) - LeetHub

---
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let count=0
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<=k+count){
+            count++
+        }
+    }
+    return k+count
+};
