2040b85bae8bd5f6de0a4be0d8900e9e1376dd5f
Mohammed Safeer <139971620+safeer239@users.noreply.github.com>
2024-11-07 17:20:34 +0530

    Time: 49 ms (84.64%), Space: 59.9 MB (73.3%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.js
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.js
@@ -4,33 +4,19 @@
  */
 
  var makeFancyString = function(s) {
-    let result = ""; // Initialize an empty string to store the result
-    let count = 1; // Initialize a counter for consecutive characters
-
+    let result = ""; 
+    let count = 1; 
     for (let i = 1; i < s.length; i++) {
         if (s[i] === s[i - 1]) {
-            count++; // Increase the counter if the current character matches the previous one
+            count++;
         } else {
-            count = 1; // Reset the counter if the character is different
+            count = 1; 
         }
-
         if (count < 3) {
-            result += s[i - 1]; // Append the previous character if it doesn't form three in a row
+            result += s[i - 1];
         }
     }
-
-    result += s[s.length - 1]; // Add the last character, as it wasn’t added in the loop
-
+    result += s[s.length - 1]; 
     return result;
 };
 
-// var makeFancyString=function(s){
-//     let splitChar=s.split("")
-//     console.log(splitChar)
-//     for(let i =0;i<splitChar.length;i++){
-//         if( splitChar[i]==splitChar[i+2]){
-//             splitChar.splice(i+2,1)
-//         }
-//     }
-//     return splitChar.join("")
-// }
